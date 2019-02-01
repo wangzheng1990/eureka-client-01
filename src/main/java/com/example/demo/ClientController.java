65d4fa25a413f82486ce6fc8afdf0ff65ce26e10
Old Won <15821777685@163.com>
2019-02-01 11:27:56 +0800

    修改成简单类名

--- src/main/java/com/example/demo/ClientController.java
+++ src/main/java/com/example/demo/ClientController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,12 @@ import java.util.List;
 public class ClientController {
 
     @Autowired
-    private org.springframework.cloud.client.discovery.DiscoveryClient discoveryClient2;
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping("/service-instance/{applicationName}")
     public List<ServiceInstance> serviceInstancesByApplicationName(
         @PathVariable String applicationName) {
-            return this.discoveryClient2.getInstances(applicationName);
+            return this.discoveryClient.getInstances(applicationName);
     }
 
 }
